e1e05af4d45259f4f7bf2d8a8ce171096b657657
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-02-06 10:29:59 -0500

    adding search function to NHL app

--- nhl-teams/script.js
+++ nhl-teams/script.js
@@ -1,13 +1,22 @@
 let getAPI = () => {
-    fetch("https://statsapi.web.nhl.com/api/v1/teams")
+    let val = document.querySelector('input').value.toLowerCase()
+
+    if (val == 'new jersey' || val == 'nj' || val == 'jersey devils' || val == 'new jersey devils' || val == 'devils') {
+        val = 1
+    } else if (val == 'new york' || val == 'new york islanders' || val == 'islanders')
+
+    console.log(val)
+
+    let url = "https://statsapi.web.nhl.com/api/v1/teams/"+val
+    console.log(url)
+    fetch(url)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-            let teamData = data.teams[Math.floor(Math.random() * 30)]
-            console.log(teamData)
-            document.querySelector('h1').innerText = `Team Name: ${teamData.name}`
-            document.querySelector('h2').innerText = `First year of play: ${teamData.firstYearOfPlay}`
+            console.log(data)
+            document.querySelector('h1').innerText = `Team Name: ${data.teams[0].name}`
+            document.querySelector('h2').innerText = `First year of play: ${data.teams[0].firstYearOfPlay}`
             document.querySelector('a').innerText = 'Team Site'
-            document.querySelector('a').href = teamData.officialSiteUrl
+            document.querySelector('a').href = data.teams[0].officialSiteUrl
         })
         .catch(err => {
             console.log(`error ${err}`)
