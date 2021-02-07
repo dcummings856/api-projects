a53947971270c26a2dc66191fd4396d26b4504fe
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-02-07 16:10:34 -0500

    work in progress - selectable teams

--- nhl-teams/script.js
+++ nhl-teams/script.js
@@ -1,43 +1,35 @@
 let getAPI = () => {
     
-    // if (val == 'new jersey' || val == 'nj' || val == 'jersey devils' || val == 'new jersey devils' || val == 'devils') {
-    //     val = 1
-    // } else if (val == 'new york' || val == 'new york islanders' || val == 'islanders')
-
-    console.log('https://statsapi.web.nhl.com/api/v1/teams')
     fetch('https://statsapi.web.nhl.com/api/v1/teams')
     .then(res => res.json()) // parse response as JSON
     .then(data => {
 
-        let val = document.querySelector('input').value.toLowerCase()
-
-        console.log(val)
-
-        console.log(data)
-        console.log(data.teams)
-        for (let i = 0; i < data.teams.length; i++) {
-        if (val == `${data.teams.name}` || val == `${data.teams.locationName}` || val == `${data.teams.shortName}` || val == `${data.teams.teamName}`) {
-            val = i
-            console.log(val)
-        }
-
-        }
+    let val = document.querySelector('select').value.toLowerCase()
+    let url = 'https://statsapi.web.nhl.com/api/v1/teams/'
+    console.log(val)
+    console.log(data)
+    if (val === 'new jersey devils') {
+        url = 'https://statsapi.web.nhl.com/api/v1/teams/1'
+    } else if (val === 'new york islands') {
+        url = 'https://statsapi.web.nhl.com/api/v1/teams/2'
+    } else if (val === 'new york rangers') {
+        url = 'https://statsapi.web.nhl.com/api/v1/teams/3'
+    }
     
-    let url = 'https://statsapi.web.nhl.com/api/v1/teams/'+val
-        console.log(url)
+    console.log(url)
+
     fetch(url)
-    
-        .then(res => res.json()) // parse response as JSON
-        .then(data => {
-            console.log(data)
-            document.querySelector('h1').innerText = `Team Name: ${data.teams.name}`
-            document.querySelector('h2').innerText = `First year of play: ${data.teams.firstYearOfPlay}`
-            document.querySelector('a').innerText = 'Team Site'
-            document.querySelector('a').href = data.teams.officialSiteUrl
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        });
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        console.log(data)
+        document.querySelector('h1').innerText = `Team Name: ${data.teams.name}`
+        document.querySelector('h2').innerText = `First year of play: ${data.teams.firstYearOfPlay}`
+        document.querySelector('a').innerText = 'Team Site'
+        document.querySelector('a').href = data.teams.officialSiteUrl
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
 
     })
     .catch(err => {
