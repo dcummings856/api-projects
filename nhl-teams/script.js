13a5a5ea22712c44c27c3bc455c5bd47fdb5a6ad
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-02-08 19:13:42 -0500

    completed function to search for teams

--- nhl-teams/script.js
+++ nhl-teams/script.js
@@ -1,36 +1,17 @@
 let getAPI = () => {
-    
-    fetch('https://statsapi.web.nhl.com/api/v1/teams')
+
+    fetch('https://statsapi.web.nhl.com/api/v1/teams/')
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-
-    let val = document.querySelector('select').value.toLowerCase()
-    let url = 'https://statsapi.web.nhl.com/api/v1/teams/'
-    console.log(val)
-    console.log(data)
-    if (val === 'new jersey devils') {
-        url = 'https://statsapi.web.nhl.com/api/v1/teams/1'
-    } else if (val === 'new york islands') {
-        url = 'https://statsapi.web.nhl.com/api/v1/teams/2'
-    } else if (val === 'new york rangers') {
-        url = 'https://statsapi.web.nhl.com/api/v1/teams/3'
-    }
-    
-    console.log(url)
-
-    fetch(url)
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-        console.log(data)
-        document.querySelector('h1').innerText = `Team Name: ${data.teams.name}`
-        document.querySelector('h2').innerText = `First year of play: ${data.teams.firstYearOfPlay}`
-        document.querySelector('a').innerText = 'Team Site'
-        document.querySelector('a').href = data.teams.officialSiteUrl
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-    });
-
+        let val = document.querySelector('select').value.toLowerCase()
+        for (let i = 0; i < data.teams.length; i++) {
+            if (data.teams[i].name.toLowerCase() === val) {
+                document.querySelector('h1').innerText = `Team Name: ${data.teams[i].name}`
+                document.querySelector('h2').innerText = `First year of play: ${data.teams[i].firstYearOfPlay}`
+                document.querySelector('a').innerText = 'Team Site'
+                document.querySelector('a').href = data.teams[i].officialSiteUrl
+            }
+        }
     })
     .catch(err => {
         console.log(`error ${err}`)
